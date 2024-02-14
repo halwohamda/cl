3f20f9194259e28a3a26632b52722d3e19ccc612
halwohamda <halwohamda@gmail.com>
2024-02-13 19:16:07 -0800

    done

--- clock/clock.js
+++ clock/clock.js
@@ -8,6 +8,6 @@ setInterval(()=>{
     let currentTime = new  Date();
   
     Hours.innerHTML = (currentTime.getHours()<10? "0":"") + currentTime.getHours();
-    Minutes.innerHTML = (currentTime.getHours()<10? "0":"") + currentTime.getMinutes();
-    Seconds.innerHTML = (currentTime.getHours()<10? "0":"") + currentTime.getSeconds();
-},1000)
+    Minutes.innerHTML = (currentTime.getMinutes()<10? "0":"") + currentTime.getMinutes();
+    Seconds.innerHTML = (currentTime.getSeconds()<10? "0":"") + currentTime.getSeconds();
+},1000);
