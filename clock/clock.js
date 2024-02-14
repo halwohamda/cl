61d185b65926cd2c36af1c0ea9d1c7d703b05f7d
halwohamda <halwohamda@gmail.com>
2024-02-13 18:20:39 -0800

    done

--- clock/clock.js
+++ clock/clock.js
@@ -3,7 +3,7 @@ let Minutes = document.getElementById('Min');
 let  Seconds = document.getElementById('Sec');
 
 let currentTime = new  Date();
- 
+ console.log(currentTime);
 // Hours.innerHTML = currentTime.getHours();
 // Minutes.innerHTML = currentTime.getMinutes();
 // Seconds.innerHTML = currentTime.getSeconds();
